57d08a7c5f4221008780727adf5497bdaa567453
nguyen-thanh-luann <imthanhluan@gmail.com>
2022-10-23 23:17:32 +0700

    update product

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -59,7 +59,7 @@ const Header = () => {
       <div className='hidden md:flex w-full h-full items-center justify-between'>
         <Link to={'/'} className='flex items-center gap-2'>
           <img src={Logo} className='w-8 object-cover' alt='logo' />
-          <p className='text-headingColor text-xl font-bold'> City</p>
+          <p className='text-headingColor text-xl font-bold'> Fstore</p>
         </Link>
 
         <div className='flex items-center gap-8'>
@@ -153,7 +153,7 @@ const Header = () => {
 
         <Link to={'/'} className='flex items-center gap-2'>
           <img src={Logo} className='w-8 object-cover' alt='logo' />
-          <p className='text-headingColor text-xl font-bold'> City</p>
+          <p className='text-headingColor text-xl font-bold'> Fstore</p>
         </Link>
 
         <div className='relative'>
